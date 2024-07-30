5a01ecc5fe711cdb4c44d82d01cb8d54a4a3f00c
Erick Ferreira <erickferreiradev@gmail.com>
2024-07-29 23:23:44 -0300

    Primeiros componentes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, SafeAreaView, StyleSheet, View } from 'react-native'
+import GlobalStyles from './components/GlobalStyles'
+
+import X, { Comp2, Comp3 } from './components/Multi'
+import Teste from './components/primeiro'
+
+export default function(){
+  return (
+  <SafeAreaView style={GlobalStyles.androidSafeArea}>
+  <View>
+      <Teste />
+      <X />
+      <Comp2 />
+      <Comp3 />
+  </View>
+  </SafeAreaView>
+  )
+}
+
---
+++ src/components/Multi.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function(){
+    return <Text>Comp OFICIAL</Text>
+}
+
+export function Comp2(){
+    return <Text>Comp 02</Text>
+}
+
+function Comp3(){
+    return <Text>Comp 03</Text>
+}
+
+export { Comp3 }
